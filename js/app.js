f4cb994b86a0234d0405cd98a4fd9018292b2e37
Kresimir I. Nurnberger <kin9006@aol.com>
2015-12-11 14:05:48 +0100

    form select option added

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
-	$('#fruit #vegetable #meat #beverages #other').submit(function(event) {
-		// alert("handler .submit called")
-		event.preventDefault();
-	})
+	// $('#fruit #vegetable #meat #beverages #other').submit(function(event) {
+	// 	// alert("handler .submit called")
+	// 	event.preventDefault();
+	// })
 	// $( "select#groceries option:selected").val();
 	// $( "select.food input:selected").val();
-	$('#groceries').change(function() {
-		// alert("its working")
-	if $('.fruit').click();
-		$('#fruit').show();
-	})	
+	// var fruitArray = ["Apples", "Oranges", "Pineapple", "Strawberries", "Bananas"];
+	// document.getElementById("fruit").innerHTML = fruitArray;
+	// $(document).click(function() {
+	// 	if $('#fruit-opt')
+	// 		$('#fruit').show();
+	// })
+	
+
 });
