1ee715b81150783a666991ebd97ae2526a74b39b
Kresimir I. Nurnberger <kin9006@aol.com>
2015-12-12 12:14:41 +0100

    dropbox option when selected div shows

--- js/app.js
+++ js/app.js
@@ -1,16 +1,30 @@
 $(document).ready(function() {
-	// $('#fruit #vegetable #meat #beverages #other').submit(function(event) {
-	// 	// alert("handler .submit called")
-	// 	event.preventDefault();
-	// })
+	$('.fruit, .vegetables, .beverages, .meat, .other').hide();
 	// $( "select#groceries option:selected").val();
 	// $( "select.food input:selected").val();
 	// var fruitArray = ["Apples", "Oranges", "Pineapple", "Strawberries", "Bananas"];
 	// document.getElementById("fruit").innerHTML = fruitArray;
-	// $(document).click(function() {
-	// 	if $('#fruit-opt')
-	// 		$('#fruit').show();
-	// })
-	
+	$('#btn1').click(function(event) {
+		// alert("it's working");
+		var choice = $('#groceries option:selected').val();
+		if (choice == 'fruit') {
+			$('.fruit').show();
+			$('.vegetables, .beverages, .meat, .other').hide();
+		}
+		else if (choice == 'vegetables') {
+			$('.vegetables').show();
+			$('.fruit, .beverages, .meat, .other').hide();
+		}
+		else if (choice == 'beverages') {
+			$('.beverages').show();
+			$('.fruit, .vegetables, .meat, .other').hide();
+		}
+		else if (choice == 'meat') {
+			$('.meat').show();
+			$('.fruit, .vegetables, .beverages, .other').hide();
+		}
+
+		event.preventDefault();
+	})
 
 });
