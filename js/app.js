43084e3634cfbeb0941db00e1f917b4d93b0d96e
Kresimir I. Nurnberger <kin9006@aol.com>
2015-12-15 18:56:38 +0100

    checkboxes show on click

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
 	// var fruitArray = ["Apples", "Oranges", "Pineapple", "Strawberries", "Bananas"];
 	// document.getElementById("fruit").innerHTML = fruitArray;
 	$('#btn1').click(function(event) {
-		// alert("it's working");
 		var choice = $('#groceries option:selected').val();
 		if (choice == 'fruit') {
 			$('.fruit').show();
@@ -23,8 +22,39 @@ $(document).ready(function() {
 			$('.meat').show();
 			$('.fruit, .vegetables, .beverages, .other').hide();
 		}
-
+		else if (choice == 'other') {
+			$('.other').show();
+			$('.fruit, .vegetables, .beverages, .meat').hide();
+		}
 		event.preventDefault();
 	})
+
+	function addToList() {
+		var shoplist = $('input:checked').val(); 
+		$('<li>').text(shoplist).appendTo('.shoplist');
+	}
+	function removeItem() {
+		var del = $('input:unchecked').val();
+		$('.shoplist').remove(del);
+	}
+	$('input').change(function() {
+		// var shoplist = $('input:checked').val(); 
+		// $('<li>').text(shoplist).appendTo('.shoplist');
+		$('form input:checked').each(function() {
+			addToList();
+		})
+		$('form input:unchecked').each(function() {
+			removeItem();
+		})
+	})
+
+	// $('.add').click(function(event) {
+	// 	$('body')
+	// 		.on('click', '.add', addToList)
+	// 		.find('input:checked')
+	// 		.text(shoplist);
+	// 	event.preventDefault();
+	// })
+	
 
 });
