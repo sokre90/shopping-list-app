bbbb41b426dca3df460be8bee410918dac1ccd70
Kresimir I. Nurnberger <kin9006@aol.com>
2015-12-18 00:22:40 +0100

    in progress

--- js/app.js
+++ js/app.js
@@ -5,56 +5,39 @@ $(document).ready(function() {
 	// var fruitArray = ["Apples", "Oranges", "Pineapple", "Strawberries", "Bananas"];
 	// document.getElementById("fruit").innerHTML = fruitArray;
 	$('#btn1').click(function(event) {
-		var choice = $('#groceries option:selected').val();
+		var choice = $('.categories option:selected').val();
 		if (choice == 'fruit') {
 			$('.fruit').show();
-			$('.vegetables, .beverages, .meat, .other').hide();
+			// $('.vegetables, .beverages, .meat, .other').hide();
 		}
 		else if (choice == 'vegetables') {
 			$('.vegetables').show();
-			$('.fruit, .beverages, .meat, .other').hide();
+			// $('.fruit, .beverages, .meat, .other').hide();
 		}
 		else if (choice == 'beverages') {
 			$('.beverages').show();
-			$('.fruit, .vegetables, .meat, .other').hide();
+			// $('.fruit, .vegetables, .meat, .other').hide();
 		}
 		else if (choice == 'meat') {
 			$('.meat').show();
-			$('.fruit, .vegetables, .beverages, .other').hide();
+			// $('.fruit, .vegetables, .beverages, .other').hide();
 		}
 		else if (choice == 'other') {
 			$('.other').show();
-			$('.fruit, .vegetables, .beverages, .meat').hide();
+			// $('.fruit, .vegetables, .beverages, .meat').hide();
+		}
+		else if (choice = 'all categories') {
+			var all = $('.fruit, .vegetables, .beverages, .meat, .other').show();
+			$(all).show();
 		}
 		event.preventDefault();
 	})
 
-	function addToList() {
-		var shoplist = $('input:checked').val(); 
-		$('<li>').text(shoplist).appendTo('.shoplist');
-	}
-	function removeItem() {
-		var del = $('input:unchecked').val();
-		$('.shoplist').remove(del);
-	}
 	$('input').change(function() {
-		// var shoplist = $('input:checked').val(); 
-		// $('<li>').text(shoplist).appendTo('.shoplist');
+		$('.shoplist').empty();
 		$('form input:checked').each(function() {
-			addToList();
-		})
-		$('form input:unchecked').each(function() {
-			removeItem();
+			var shoplist = $(this).val(); 
+			$('<li>').text(shoplist).appendTo('.shoplist');
 		})
 	})
-
-	// $('.add').click(function(event) {
-	// 	$('body')
-	// 		.on('click', '.add', addToList)
-	// 		.find('input:checked')
-	// 		.text(shoplist);
-	// 	event.preventDefault();
-	// })
-	
-
-});
+})
